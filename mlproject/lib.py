d866a067497145d1e59cd02ed3ade87d2c0d77e9
Lisaaa2021 <88220244+Lisaaa2021@users.noreply.github.com>
2021-11-27 16:11:51 +0800

    3.0 version?

--- mlproject/lib.py
+++ mlproject/lib.py
@@ -1,9 +1,9 @@
 def hello_world():
     return "Hello world from mlproject"
 
-def try_me():
-    return 'The matrix 4 is going to be released in December!'
+# def try_me():
+#     return 'The matrix 4 is going to be released in December!'
 
-def try_me2():
-    x = input("Please enter your name")
+def try_me():
+    x = input("Please enter your name: ")
     return f'Hi {x}, do you know your real identity is an AI developed by Le Wagon DS batch 708?'
